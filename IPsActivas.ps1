e51b25e0a6d52b28a83305d663c176eebca4af27
bautistas21 <bautistas21@outlook.com>
2025-05-23 17:55:05 -0600

    Update IPsActivas.ps1

--- IPsActivas.ps1
+++ IPsActivas.ps1
@@ -1,13 +1,14 @@
-﻿# Obtener todas las conexiones de red activas
+# Obtener todas las conexiones de red activas
 $connections = Get-NetTCPConnection | Select-Object -ExpandProperty RemoteAddress
 
 # Filtrar para eliminar valores vacíos y evitar repetir IPs
 $uniqueIPs = $connections | Where-Object { $_ -match "\d+\.\d+\.\d+\.\d+" } | Sort-Object -Unique
 
+$uniqueIPs | Out-String
 # Imprimir la lista de IPs establecidas
-Write-Host "Lista de IPs conectadas al equipo:" $uniqueIPs 
+#Write-Host "Lista de IPs conectadas al equipo:" $uniqueIPs 
 
 # Opcional: Guardar la lista en un archivo de texto
-$uniqueIPs | Out-File -FilePath "ConexionesIPs.txt"
+#$uniqueIPs | Out-File -FilePath "ConexionesIPs.txt"
 
-Write-Host "El listado de IPs también ha sido guardado en 'ConexionesIPs.txt'"
+#Write-Host "El listado de IPs también ha sido guardado en 'ConexionesIPs.txt'"
